887d31cf70d43122c6ccdccdfbca3aaf9067b959
Danara Tagayeva <danara.tagayeva@gmail.com>
2022-10-24 23:35:04 -0400

    header component created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import Header from './components/Header';
+import { Component } from 'react';
 
-function App() {
-  return (
-    <div className='App'>
-      <h1> Hello from React!</h1>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className='App'>
+//       <Header />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  render() {
+    return <Header />;
+  }
 }
 
 export default App;
